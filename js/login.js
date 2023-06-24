eab9700ea5d048eaa42852c1b64063bbfe9b15a0
Nauy2004 <117803022+Nauy2004@users.noreply.github.com>
2023-06-24 19:51:24 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,30 +1,5 @@
-let login = document.getElementById("login");
-let regetPass = document.getElementById("reget_password");
-let btnRegotPass = document.getElementById("btn__reget-password");
-let register = document.getElementById("register");
-let btnRegister = document.getElementById("btn__register");
-let btnLogin = document.getElementById("btn__login");
 const formEle = document.getElementById("register__form");
 let loginEle = document.getElementById("login-form");
-function toRegetPass() {
-    login.style.display = 'none';
-    regetPass.style.display = 'block';
-    register.style.display = 'none';
-}
-function toRegister() {
-    login.style.display = 'none';
-    regetPass.style.display = 'none';
-    register.style.display = 'block';
-}
-function toLogin() {
-    login.style.display = 'block';
-    regetPass.style.display = 'none';
-    register.style.display = 'none';
-}
-
-btnRegotPass.addEventListener("click",toRegetPass);
-btnRegister.addEventListener('click',toRegister);
-btnLogin.addEventListener('click',toLogin);
 
 
 formEle.addEventListener("submit", (e) => {
