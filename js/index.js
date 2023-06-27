b60220975ea7c62073c6460c8eaaa5ca1936ecce
HoangQuangKy <kyhoang487@gmail.com>
2023-06-27 15:25:40 +0700

    index add some item

--- js/index.js
+++ js/index.js
@@ -8,42 +8,42 @@ let header = document.getElementsByClassName("header__nav-bottom")[0];
 let sticky = header.offsetTop;
 //-------------Function---------------
 function hidden() {
-    loginBoxEle.classList.toggle("d-none");
-    socialMediaEle.classList.toggle("d-none-special");
-    };
+  loginBoxEle.classList.toggle("d-none");
+  socialMediaEle.classList.toggle("d-none-special");
+};
 function refresh() {
-    location.reload();
+  location.reload();
 }
 function upToHeader() {
-    window.scrollTo({
-        top:0,
-        behavior: 'smooth'
-    })
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
 }
 function fixed() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 //-------------------------------------
 
 
-personIconEle.addEventListener("click", hidden); 
-bigContent.addEventListener("click",refresh);
+personIconEle.addEventListener("click", hidden);
+bigContent.addEventListener("click", refresh);
 
 for (let i = 0; i < discountInformationItemEle.length; i++) {
-    discountInformationItemEle[i].addEventListener('click',upToHeader)
+  discountInformationItemEle[i].addEventListener('click', upToHeader)
 }
 
-window.onscroll = function() {fixed()};
+window.onscroll = function () { fixed() };
 
-var splide = new Splide( '.splide', {
-    type   : 'loop',
-    drag   : 'free',
-    perPage: 3,
-  } );
+var splide = new Splide('.splide', {
+  type: 'loop',
+  drag: 'free',
+  perPage: 3,
+});
 splide.mount();
 
 var productItems = document.getElementsByClassName("product-item");
@@ -80,33 +80,33 @@ setTimeout(demo, 3050)
 
 var mousein = false
 function demo() {
-   if(mousein) return
-   document.getElementById('demo' + count++)
-      .classList.toggle('hover')
-   
+  if (mousein) return
+  document.getElementById('demo' + count++)
+    .classList.toggle('hover')
+
 }
 
 function demo2() {
-   if(mousein) return
-   document.getElementById('demo2')
-      .classList.toggle('hover')
+  if (mousein) return
+  document.getElementById('demo2')
+    .classList.toggle('hover')
 }
 
 function reset() {
-   count = 1
-   var hovers = document.querySelectorAll('.hover')
-   for(var i = 0; i < hovers.length; i++ ) {
-      hovers[i].classList.remove('hover')
-   }
+  count = 1
+  var hovers = document.querySelectorAll('.hover')
+  for (var i = 0; i < hovers.length; i++) {
+    hovers[i].classList.remove('hover')
+  }
 }
 
-document.addEventListener('mouseover', function() {
-   mousein = true
-   reset()
+document.addEventListener('mouseover', function () {
+  mousein = true
+  reset()
 })
 let menuButtonEle = document.getElementById("click-img");
 let noneELe = document.getElementsByClassName("none")[0];
-menuButtonEle.addEventListener("click",() => {
+menuButtonEle.addEventListener("click", () => {
   noneELe.classList.toggle("none");
   noneELe.classList.toggle("content-none")
 })
@@ -114,7 +114,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -127,4 +127,7 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
+}
+function redirectToPage(pageUrl) {
+  window.location.href = pageUrl;
 }
