7da1d388f07e969d340535de18869ded46eb5870
HoangQuangKy <kyhoang487@gmail.com>
2023-06-27 10:46:52 +0700

    JS of range price

--- js/login.js
+++ js/login.js
@@ -26,30 +26,3 @@ function isLoginPage(list, email, password) {
     }
     return isLogin = false;
 }
-loginEle.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let email = e.target.email.value;
-    let password = e.target.password.value;
-
-    const listOfEmail = localStorage.getItem("accounts")
-        ? JSON.parse(localStorage.getItem("accounts"))
-        : [];
-
-    const isLogin = isLoginPage(listOfEmail, email, password);
-    if (isLogin) {
-        alert("Đăng nhập thành công");
-    } else {
-        alert("Đăng nhập thất bại");
-    }
-});
-
-function isLoginPage(list, email, password) {
-    let isLogin = false;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].email === email && list[i].password === password) {
-            isLogin = true;
-            break;
-        }
-    }
-    return isLogin;
-}
