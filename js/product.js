f8060a00c0582cd1f6bbdaa4126cc7f406e1949d
minhquy0898 <minhquy43123@gmail.com>
2023-06-29 18:51:56 +0700

    update

--- js/product.js
+++ js/product.js
@@ -2,78 +2,79 @@ var text = {
     contact: [{
         productTitle: "Nhẫn ngọc trai cao cấp",
         productPrice: 998.000,
-        image: './img/nhan-doi-kim-cuong.webp'
+        image: './imgs/blog/nhan-ngoc-trai.webp'
     },
     {
         productTitle: "Vòng tay cao cấp",
         productPrice: 500.000,
-        image: './img/VONG_TAY_CC.webp'
+        image: './imgs/blog/vong-tay-bk.webp'
     },
     {
         productTitle: "Nhẫn vàng cao cấp",
         productPrice: 400.000,
-        image: './img/nhan-vang.webp'
+        image: './imgs/blog/nhan-vang.webp'
     },
     {
         productTitle: "Nhẫn kim cương cao cấp",
         productPrice: 700.000,
-        image: './img/nhan-kim-cuong.webp'
+        image: './imgs/blog/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn tình yêu",
         productPrice: 900.000,
-        image: './img/nhan-tinh-yeu.webp'
+        image: './imgs/blog/nhan-tinh-yeu.webp'
     },
     {
         productTitle: "Bông tai cao cấp",
         productPrice: 500.000,
-        image: './img/bongtaicc.webp'
+        image: './imgs/blog/bong-tai-cc.webp'
     },
     {
         productTitle: "Bông tai ngọc trai",
         productPrice: 600.000,
-        image: './img/bong-tai-ngoc-trai.webp'
+        image: './imgs/blog/bong-tai-ngoc-trai.webp'
 
     },
     {
         productTitle: "Nhẫn đôi kim cương",
         productPrice: 910.000,
-        image: './img/nhan-doi-kim-cuong.webp'
+        image: './imgs/blog/nhan-kim-cuong.webp'
     },
     {
         productTitle: "Chìa khóa tình yêu",
         productPrice: 400.000,
-        image: './img/chia-khoa-TY.webp'
+        image: './imgs/blog/chia-khoa-TY.webp'
     },
     {
         productTitle: "Dây chuyền số 8",
         productPrice: 230.000,
-        image: './img/day-duyen-so8.webp'
+        image: './imgs/blog/day-duyen-so8.webp'
     },
     {
         productTitle: "Vòng cổ đá xanh",
         productPrice: 300.000,
-        image: './img/vong-co.webp'
+        image: './imgs/blog/vong-co.webp'
     },
     {
         productTitle: "Nhẫn vòng basis",
         productPrice: 400.000,
-        image: './img/basis.webp'
+        image: './imgs/blog/basis.webp'
     },
     {
         productTitle: "Bông tai phá cách",
         productPrice: 740.000,
-        image: './img/bong-tai-pha-cach.webp'
+        image: './imgs/blog/bong-tai-pha-cach.webp'
     },
     {
         productTitle: "Vòng tay tình nhân",
         productPrice: 690.000,
-        image: './img/vong-tay-tinh-nhan.webp'
+        image: './imgs/blog/vong-tay-tinh-nhan.webp'
     },
     {
         productTitle: "Nhẫn vòng đặc biệt",
         productPrice: 999.000,
-        image: './img/nhan-hong-KT.webp'
+        image: './imgs/blog/nhan.webp'
     },
     ]
 }
+
