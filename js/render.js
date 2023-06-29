0586ac88a7b51766e5b47d622d87d4ec50947a2e
Nauy2004 <tuanlmht77@gmail.com>
2023-06-29 14:38:15 +0700

    fix product

---
+++ js/render.js
@@ -0,0 +1,42 @@
+let cardEle = document.querySelector(".row-product-view");
+console.log(cardEle);
+
+window.onload = () => {
+
+    fetch("https://649c5833048075719237fecf.mockapi.io/san-pham")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        render(data);   
+    })
+    .catch((err) => console.log(err));
+};
+
+function render(listItems) {
+    totalHtml ="";
+    for (let index of listItems) {
+        let rawHTML = `
+        <div class="product-grid">
+            <div class="product-meadi">
+                <a href="">
+                    <img src="${index.avatar}" alt="">
+                </a>
+                <div class="overlay">
+                    <div class="overlay-icon">
+                        <i class="fa-solid fa-bag-shopping"></i>
+                    </div>
+                </div>
+                <div class="sale">
+                    <a href="">sale</a>
+                </div>
+            </div>
+            <div class="product--info">
+                <div class="product-title">${index.name}</div>
+                <div class="product-price">${index.sell}</div>
+            </div>
+        </div>
+       `;       
+       totalHtml += rawHTML;
+    };
+    cardEle.innerHTML = totalHtml;
+}
